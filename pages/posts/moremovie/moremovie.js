09ad9815bb25a1b740ae69160d9332063ed0e62d
DengXiangshuai <40018621+DengXiangshuai@users.noreply.github.com>
2018-06-10 10:29:45 +0800

    right

--- pages/posts/moremovie/moremovie.js
+++ pages/posts/moremovie/moremovie.js
@@ -8,13 +8,6 @@ Page({
     var that = this;
     var category = options.category;
     this.data.navigateTitle = category;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    var dataurl = "http://t.yushu.im" + "/v2/movie/in_theaters";
-  
-=======
-=======
->>>>>>> parent of 3b1caa1... a
     var dataurl = "";
     switch ("category") {
       case "北美热映":
@@ -27,10 +20,6 @@ Page({
         dataurl = "http://t.yushu.im" + "/v2/movie/top250";
         break;
     }
-<<<<<<< HEAD
->>>>>>> parent of 3b1caa1... a
-=======
->>>>>>> parent of 3b1caa1... a
 
     wx.setNavigationBarTitle({
       title: this.data.navigateTitle
